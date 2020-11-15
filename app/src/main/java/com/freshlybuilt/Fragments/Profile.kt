9f0880df19a11721176c0808957132287380663d
ravi pawar <ravipawar5175@gmail.com>
2020-11-15 21:00:48 +0530

    google login integrated
    Signed-off-by: ravi pawar <ravipawar5175@gmail.com>

--- app/src/main/java/com/freshlybuilt/Fragments/Profile.kt
+++ app/src/main/java/com/freshlybuilt/Fragments/Profile.kt
@@ -1,19 +1,57 @@
 package com.freshlybuilt.Fragments
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.freshlybuilt.Login
 
 import com.freshlybuilt.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.tasks.OnCompleteListener
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class Profile : Fragment(R.layout.fragment_profile) {
 
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val userAcc = GoogleSignIn.getLastSignedInAccount(activity)
+        var profileName = userAcc!!.displayName
+        var profilePhoto = userAcc!!.photoUrl
+        user_name.setText(profileName)
+        Glide.with(this).load(profilePhoto).into(user_image)
+
+
+
+        log_out.setOnClickListener(){
+            signOut()
+        }
+
+    }
+
+    private fun signOut(){
+        val gsoCall = Login().gso
+        val mGoogleSignInClient = GoogleSignIn.getClient(activity!!, gsoCall)
+        val iLogin : Intent = Intent(activity,Login::class.java)
+        mGoogleSignInClient.signOut()
+        startActivity(iLogin)
+
+    }
 
 
 }
---
+++ app/src/main/java/com/freshlybuilt/Login.kt
@@ -0,0 +1,89 @@
+package com.freshlybuilt
+
+import android.R.attr
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class Login : AppCompatActivity() {
+    private val RC_SIGN_IN : Int = 0
+
+
+
+    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account == null){
+            inAppNotification("please login to continue")
+        }
+        else{
+            val iBase : Intent = Intent(this, Base::class.java)
+            startActivity(iBase)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        button_google_login.setOnClickListener{
+            signIn()
+
+
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+
+
+    private fun signIn(){
+
+        val mGoogleSignInClient : GoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            val iBase : Intent = Intent(this, Base::class.java)
+            startActivity(iBase)
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(FragmentActivity.TAG, "signInResult:failed code=" + e.statusCode)
+            //updateUI(null)
+            inAppNotification(e.statusCode.toString())
+        }
+    }
+
+    private fun inAppNotification(arg :String){
+        val tTime : Int = Toast.LENGTH_LONG
+        val toast : Toast = Toast.makeText(applicationContext,arg,tTime)
+        toast.show()
+    }
+}
