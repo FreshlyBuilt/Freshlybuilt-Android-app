b43bc3caccb2e0d057840c514792de2ff4f2098e
ravi pawar <ravipawar5175@gmail.com>
2020-11-25 21:03:06 +0530

    manual login completed

---
+++ app/src/main/java/com/freshlybuilt/Modals/User.kt
@@ -0,0 +1,20 @@
+package com.freshlybuilt.Modals
+
+object User{
+
+    var LoggedInState : Int = 0
+
+    var FullName : String = "default_name"
+    var FirstName : String = "default_first_name"
+    var LastName : String = "default_last_name"
+    var UserName : String = "default_user_name"
+    var UserId : String = "default_user_id"
+    var UserEmail : String = "default_user_email"
+    var UserAvatar : String = "default_user_avatar"
+    var UserPoints : Int = 0
+    var UserDescription : String = "default_user_description"
+    var UserCoverPhoto : String = "default_user_cover"
+    var UserCapabilities : String = "default_user_capabilities"             //list type
+    var UserRegisteredOn : String = "default_user_registration_timestamp"
+
+}
--- app/src/main/java/com/freshlybuilt/Register.kt
+++ app/src/main/java/com/freshlybuilt/Register.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.view.View
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 import kotlinx.android.synthetic.main.activity_register.*
 
@@ -15,11 +18,26 @@ class Register : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+
         know_more_full_name.setOnClickListener{
             KnowMore("Full_Name_Desc")
         }
 
+        button_user_register.setOnClickListener {
+            if (Full_Name_text.text!!.isEmpty() ||
+                    user_name_text.text!!.isEmpty() ||
+                    email_text.text!!.isEmpty() ||
+                        password_text.text!!.isEmpty() ||
+                        c_password_text.text!!.isEmpty()){
+            }else{
 
+                if (c_password_text.text.toString() != password_text.text.toString()){
+
+                }else{
+                    UserFirebaseAuth()
+                }
+            }
+        }
     }
 
     private fun KnowMore(know_more_id : String){
@@ -35,6 +53,29 @@ class Register : AppCompatActivity() {
                 Full_Name_Desc.visibility = View.GONE
             }
         }
+
+    }
+
+    private fun UserFirebaseAuth(){
+        val auth = Firebase.auth
+
+        auth.createUserWithEmailAndPassword(email_text.text.toString(), password_text.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    //Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    //Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, task.exception.toString(),
+                        Toast.LENGTH_LONG).show()
+                    //updateUI(null)
+                }
+
+                // ...
+            }
 
     }
 
