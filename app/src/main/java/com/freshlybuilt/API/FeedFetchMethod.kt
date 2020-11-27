eab56ad5fee63f36019ccf0a1e27bc3a360afec7
ravi pawar <ravipawar5175@gmail.com>
2020-11-27 20:22:24 +0530

    added feed recycler

---
+++ app/src/main/java/com/freshlybuilt/API/FeedFetchMethod.kt
@@ -0,0 +1,38 @@
+package com.freshlybuilt.API
+
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.freshlybuilt.Login
+import org.json.JSONException
+import org.json.JSONObject
+
+object FeedFetchMethod {
+
+     lateinit var responseFetch : JSONObject
+
+    val API_CALL_FETCH_POST : String = "https://freshlybuilt.com/api/get_recent_posts/?page=4"
+
+    fun PostFetch() : StringRequest{
+        val requestPostFetch : StringRequest = StringRequest(Request.Method.GET, API_CALL_FETCH_POST,
+            Response.Listener<String> { response ->
+                try{
+                    val responseJson : JSONObject = JSONObject(response)
+                    if (responseJson["status"]=="ok"){
+                        Log.d("fetch","success")
+                        responseFetch = JSONObject(response)
+                    }
+                    else{
+                        Log.d("fetch","failed")
+                    }
+                }catch (e : JSONException) {
+                    Log.d("fetch","exception")
+                }
+            }, Response.ErrorListener {
+                Log.d("fetch","fetch error")
+            })
+        return requestPostFetch
+    }
+}
